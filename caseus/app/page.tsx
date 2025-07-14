f3353649afbae22b99427f1b1a33e5196af58aa8
Illia <ilya.merinec@gmail.com>
2025-07-15 00:16:20 +0200

    fix

--- caseus/app/page.tsx
+++ caseus/app/page.tsx
@@ -7,8 +7,12 @@ import AnimatedNavbar from "@/components/AnimatedNavbar";
 import SplitText from "gsap/SplitText";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
+import { PiArrowRightThin } from "react-icons/pi";
+
 
 gsap.registerPlugin(SplitText,ScrollTrigger)
+gsap.registerPlugin(ScrollTrigger);
+
 
 export default function Home() {
   const lenisRef = useRef<any>(null);
@@ -16,7 +20,8 @@ export default function Home() {
   const heroTextRef = useRef(null);
   const videoRef = useRef(null);
   const videoWrapper = useRef(null);
-  
+  const heroRef = useRef(null);
+const section2Ref = useRef(null);
 
     useEffect(() => {
     if (typeof window !== "undefined" && textRef.current) {
@@ -55,17 +60,20 @@ export default function Home() {
 
 
 
+
+
+
    useEffect(() => {
     gsap.registerPlugin(ScrollTrigger);
     const isMobile = window.innerWidth < 768;
-    const heroHeight = isMobile ? 1000 : 2500;
+    const heroHeight = isMobile ? 1900 : 3000;
     const split = new SplitText(heroTextRef.current, {
-  type: "lines,words"
+  type: "lines,chars"
 });
 
     const tl = gsap.timeline({
       scrollTrigger: {
-        trigger: "#hero",
+        trigger: "#scroll-container",
         start: "top top",
         end: heroHeight,
         scrub: true,
@@ -100,28 +108,71 @@ export default function Home() {
   
   },
   {duration:1,ease: "power2.out", y: -500,},
-  "-=0.9"
-).fromTo(
-  split.words,
-  { color: "#DEE2E6" },
-  {
-    color: "#000000",
-    stagger: 0.03,
-    },
-    "-=0.7"
-);
+  "+=0.5"
+)
+.fromTo(
+  split.lines,
+  { y:-500 },
+  { y:-600, stagger:0.5},
+   
+)
+.fromTo(
+  heroRef.current,
+  {y:0},
+  {y:-500, duration:1},
+  "+=0"
+)
+// section change animation
+.fromTo(
+  section2Ref.current,
+  {y:0},
+  {y:-200, duration:1},
+  "-=0.5"
+)
+.fromTo(
+  section2Ref.current,
+  {y:-200},
+  {y:-1000, duration:2},
+  "+=0"
+)
+
+
+
+
+
+
+// text color change animation
+gsap.timeline({
+  scrollTrigger: {
+    trigger: "#animated-text",
+    start: "top 20%",    // текст появляется при входе в вьюпорт
+    end: "+=500",        // сколько скролла на анимацию
+    scrub: true,
+    markers: false,       // убери потом
+  },
+})
+.to(split.chars, {
+  color: "#000",
+  stagger: 0.05,
+})
+
+
 
   }, []);
 
+
   
+
 
   return (
     <>
       <AnimatedNavbar />
 
       <ReactLenis root options={{ autoRaf: true }} ref={lenisRef} />
-      {/* HERO SECRION */}
-      <section className="h-screen w-full " id="hero">
+
+      <div id="scroll-container">
+{/* HERO SECRION */}
+      <section className="h-screen w-full " id="hero" ref={heroRef}>
         <div className="relative p-0 m-0 ">
           {/* Video hero section */}
           <div className="w-full h-screen z-0" ref={videoRef}>
@@ -155,30 +206,40 @@ export default function Home() {
 
         </div>
         {/* animated text hero section */}
-        <div className=" max-w-[70%] md:max-w-[70%] pl-5" ref={heroTextRef} id="suka">
+        <div className=" max-w-[70%] md:max-w-[70%] pl-5" ref={heroTextRef} id="animated-text">
         <p className="text-gray-400 text-4xl z-2 md:text-6xl">Combining creativity and expertise, we reveal the character of brands that stand out in their time.</p>
         </div>
       </section>
-
-      <section className="min-h-screen w-full mx-auto px-5">
-        <div className="grid grid-rows-3 md:grid-rows-4 gap-5 md:grid-cols-2 ">
-        <div className="relative group rounded-2xl overflow-hidden md:col-span-1 md:row-span-1">
-          <img src="photo2.jpg" alt="" className="block w-full rounded-2xl transition-opacity duration-300 group-hover:opacity-0" />
-          <video src="video1.mp4" className=" absolute inset-0 w-full h-full opacity-0 object-cover group-hover:opacity-100 rounded-2xl transition-opacity duration-300" autoPlay muted loop onMouseEnter={e => e.currentTarget.play()} onMouseLeave={e => { e.currentTarget.pause(); e.currentTarget.currentTime = 0; }} />
-          <div className="absolute bottom-4 left-4"><p className="text-white">Your Button or Text</p></div>
+{/* project section */}
+      <section className="min-h-screen w-full mx-auto lg:px-20 px-5" id="section2" ref={section2Ref}>
+        <div className="grid grid-rows-3 md:grid-rows-4  md:grid-cols-2 lg:gap-7 gap-5">
+        <div className="relative group rounded-2xl overflow-hidden md:col-span-1 md:row-span-1 cursor-pointer ">
+          <img src="photo2.jpg" alt="" className="  block w-full rounded-2xl transition-opacity duration-300 group-hover:opacity-0" />
+          <video src="video1.mp4" className="  absolute inset-0 w-full h-full opacity-0 object-cover group-hover:opacity-100 rounded-2xl transition-opacity duration-300" autoPlay muted loop onMouseEnter={e => e.currentTarget.play()} onMouseLeave={e => { e.currentTarget.pause(); e.currentTarget.currentTime = 0; }} />
+          <div className="absolute bottom-4 left-4 bg-white px-5 pt-3 pb-3 rounded-xl flex flex-row gap-3"><p className="text-black  font-semibold text-2xl m-auto">Autotransfer</p> 
+          <div className="inline-flex items-center justify-center rounded-full bg-gray-100  transition-colors duration-550 ease-in-out group-hover:bg-black p-2 md:p-3">
+  <PiArrowRightThin className="text-xl text-black group-hover:text-white transition-colors duration-550 ease-in-out " />
+</div></div>
         </div>
-        <div className="relative group rounded-2xl overflow-hidden md:col-span-1  md:row-span-1" >
+        <div className="relative group rounded-2xl overflow-hidden md:col-span-1 md:row-span-1 cursor-pointer ">
           <img src="photo1.jpg" alt="" className="block w-full rounded-2xl transition-opacity duration-300 group-hover:opacity-0" />
-          <video src="video2.mp4" className=" absolute inset-0 w-full h-full  opacity-0 object-cover group-hover:opacity-100 rounded-2xl transition-opacity duration-300" autoPlay muted loop onMouseEnter={e => e.currentTarget.play()} onMouseLeave={e => { e.currentTarget.pause(); e.currentTarget.currentTime = 0; }} />
-          <div className="absolute bottom-4 left-4"><p className="text-white">Your Button or Text</p></div>
+          <video src="video2.mp4" className=" absolute inset-0 w-full h-full opacity-0 object-cover group-hover:opacity-100 rounded-2xl transition-opacity duration-300" autoPlay muted loop onMouseEnter={e => e.currentTarget.play()} onMouseLeave={e => { e.currentTarget.pause(); e.currentTarget.currentTime = 0; }} />
+          <div className="absolute bottom-4 left-4 bg-white px-5 pt-3 pb-3 rounded-xl flex flex-row gap-3"><p className="text-black  font-semibold text-2xl m-auto">Autotransfer</p> 
+          <div className="inline-flex items-center justify-center rounded-full bg-gray-100  transition-colors duration-550 ease-in-out group-hover:bg-black p-2 md:p-3">
+  <PiArrowRightThin className="text-xl text-black group-hover:text-white transition-colors duration-550 ease-in-out " />
+</div></div>
         </div>
-        <div className="relative group rounded-2xl overflow-hidden md:col-span-2 md:h-[45%]  md:row-span-2">
+        <div className="md:max-h-[45%] relative group rounded-2xl overflow-hidden md:col-span-2 md:row-span-2 cursor-pointer ">
           <img src="photo3.jpg" alt="" className="block w-full rounded-2xl transition-opacity duration-300 group-hover:opacity-0" />
-          <video src="video3.mp4" className="absolute inset-0 w-full h-full object-cover opacity-0 group-hover:opacity-100 rounded-2xl transition-opacity duration-300" autoPlay muted loop onMouseEnter={e => e.currentTarget.play()} onMouseLeave={e => { e.currentTarget.pause(); e.currentTarget.currentTime = 0; }} />
-          <div className="absolute bottom-4 left-4"><p className="text-white">Your Button or Text</p></div>
+          <video src="video3.mp4" className=" absolute inset-0 w-full h-full opacity-0 object-cover group-hover:opacity-100 rounded-2xl transition-opacity duration-300" autoPlay muted loop onMouseEnter={e => e.currentTarget.play()} onMouseLeave={e => { e.currentTarget.pause(); e.currentTarget.currentTime = 0; }} />
+          <div className="absolute bottom-4 left-4 bg-white px-5 pt-3 pb-3 rounded-xl flex flex-row gap-3"><p className="text-black  font-semibold text-2xl m-auto">Autotransfer</p> 
+          <div className="inline-flex items-center justify-center rounded-full bg-gray-100  transition-colors duration-550 ease-in-out group-hover:bg-black p-2 md:p-3">
+  <PiArrowRightThin className="text-xl text-black group-hover:text-white transition-colors duration-550 ease-in-out " />
+</div></div>
         </div>
         </div>
       </section>
+      </div>
       <section className="h-screen w-full"></section>
     </>
   );
