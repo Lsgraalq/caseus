e4558dfdcf25ecfbebc3a6a7cf2414f6d9b89500
lsgraalq <Ilya.merinec@gmail.com>
2025-07-30 23:28:01 +0300

    final version navbar glassmorphism style

--- components/AnimatedNavbar.tsx
+++ components/AnimatedNavbar.tsx
@@ -81,23 +81,23 @@ export default function AnimatedNavbar() {
 
   return (
     <>
-    <nav className={`fixed top-0 left-0 w-full flex justify-center items-start z-50 text-white transition-transform duration-550  ${
+    <nav className={`fixed top-0 left-0 w-full flex justify-center items-start z-50 text-black transition-transform duration-550  ${
         show ? "translate-y-0" : "-translate-y-full"
       }`}>
              
              
                 <div
                   ref={circletwoRef}
-                  className="absolute w-12 h-12  rounded-full  text-white text-xl font-bold  mt-10 z-1000 "
+                  className="absolute w-12 h-12  rounded-full  text-black text-xl font-bold  mt-10 z-1000 "
                 >
                   <img src="/logo.png" alt=""  className="z-1000  "/>
                 </div>
-            
-      <div className="relative flex flex-row  mt-8 ml-5 pl-5 pr-5 pt-2 pb-2 rounded-2xl  items-center gap-8 glassnav" ref={navbarRef}>
+      
+      <div className="relative flex flex-row  mt-8 ml-5 pl-5 pr-5 pt-2 pb-2 border-white/20 border rounded-2xl  items-center gap-8 glassnav" ref={navbarRef}>
         {/* Круг */}
         {/* <div
            ref={circleRef}
-          className=" w-12 h-12 bg-gray-600 rounded-full  text-white text-xl font-bold z-20"
+          className=" w-12 h-12 bg-gray-600 rounded-full  text-black text-xl font-bold z-20"
         >
           <img src="/logo.png" alt=""  className="z-20"/>
         </div> */}
@@ -107,41 +107,42 @@ export default function AnimatedNavbar() {
         
           <a
             href="#"
-            className="hidden md:flex text-white cursor-pointer relative group px-2"
+            className="hidden md:flex text-black cursor-pointer relative group px-2"
           >
             Home
-            <span className="absolute bottom-0 left-0 h-[2px] w-0 bg-amber-100 transition-all group-hover:w-full"></span>
+            <span className="absolute bottom-0 left-0 h-[2px] w-0 bg-black transition-all group-hover:w-full"></span>
           </a>
 
           <a
             href="#"
-            className="hidden md:flex text-white cursor-pointer relative group px-2"
+            className="hidden md:flex text-black cursor-pointer relative group px-2"
           >
             About
-            <span className="absolute bottom-0 left-0 h-[2px] w-0 bg-amber-100 transition-all group-hover:w-full"></span>
+            <span className="absolute bottom-0 left-0 h-[2px] w-0 bg-black transition-all group-hover:w-full"></span>
           </a>
 
           <a
             href="#"
-            className="hidden md:flex text-white cursor-pointer relative group px-2"
+            className="hidden md:flex text-black cursor-pointer relative group px-2"
           >
             Contact
-            <span className="absolute bottom-0 left-0 h-[2px] w-0 bg-amber-100 transition-all group-hover:w-full"></span>
+            <span className="absolute bottom-0 left-0 h-[2px] w-0 bg-black transition-all group-hover:w-full"></span>
           </a>
 
 
-          <div className="bg-white flex flex-row   items-center  gap-2 text-center  px-5 pt-3 pb-3 rounded-xl md:hidden" onClick={toggleMenu}>
+          <div className=" flex flex-row   items-center  gap-2 text-center  px-5 pt-3 pb-3 rounded-xl md:hidden glassbutton" onClick={toggleMenu}>
             {isMenuOpen ? <IoMdClose className="w-5 text-black" /> : <IoIosMenu className="w-5 flex text-black" />}
             <p className="text-black font-semibold">Menu</p>
           </div>
-           <div className=" flex-row  group items-center  gap-2 text-center  px-8 pt-2 pb-3  rounded-xl hidden md:flex  transition duration-400 glassbutton" onClick={toggleMenu}>
+           <div className=" flex-row  group items-center  gap-2 text-center  px-8 pt-2 pb-3  rounded-xl hidden md:flex  transition duration-400 glassbutton " onClick={toggleMenu}>
             
-            <p className="text-white group-hover:text-black  font-semibold transition-colors duration-300 cursor-pointer ">Contakt</p>
+            <p className="text-black group-hover:text-black  font-semibold transition-colors duration-300 cursor-pointer ">Contakt</p>
             
           </div>
 
     
       </div>
+      
       
 
     </nav>
@@ -152,11 +153,11 @@ export default function AnimatedNavbar() {
             <div
               key={i}
              ref={(el) => (linksRef.current[i] = el, undefined)}
-              className="glassmenu z-100 rounded-2xl mx-auto flex"
+              className="glassmenu z-100 rounded-2xl mx-auto flex border-white/20 border"
             >
               <a
                 href="#"
-                className="text-white px-10 pt-8 pb-8 text-4xl rounded-2xl"
+                className="text-black px-10 pt-8 pb-8 text-4xl rounded-2xl"
               >
                 {text}
               </a>
